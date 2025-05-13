76cc2d55bbe154587605ccef0680e1f217855d12
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-12 21:03:17 -0300

    refactor: update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+
+const userRoutes = require('./routes/userRoutes');
+const gameRoutes = require('./routes/gameRoutes');
+
 const app = express();
 
+const corsOptions = {
+  origin: '*',
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Sudoku Server!');
+});
+
+app.use('/api/users', userRoutes);
+app.use('/api/games', gameRoutes);
 
 module.exports = app;
