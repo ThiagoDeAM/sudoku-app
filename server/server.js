a7e65778b78b6e1bdb53fe392f5a5a450f9618b4
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-12 21:06:48 -0300

    refactor: port updated

--- server/server.js
+++ server/server.js
@@ -1,26 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
 const app = require('./src/app');
-const userRoutes = require('./src/routes/userRoutes');
-const gameRoutes = require('./src/routes/gameRoutes');
+const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: '*',
-  //origin: 'http://localhost:3000',
-};
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Sudoku Server!');
-});
-
-app.use('/api/users', userRoutes);
-app.use('/api/games', gameRoutes);
-
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
